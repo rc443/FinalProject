41088e5eb2e1d785dca3a9f2698feacc561b9700
Rafael Correa <rc443@njit.edu>
2017-08-06 16:24:45 -0400

    Fixed some layout issues

--- previous_to_do_lists/view/footer.php
+++ previous_to_do_lists/view/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <p class="copyright">
-        &copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.
+        &copy; <?php echo date("Y"); ?> My To Do List Items.
     </p>
 </footer>
 </body>
