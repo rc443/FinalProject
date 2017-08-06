55087e1c3f33f0cac8f0ba560c037a662d60b2e7
Rafael Correa <rc443@njit.edu>
2017-08-06 16:24:23 -0400

    Fixed some layout issues

--- previous_to_do_lists/view/header.php
+++ previous_to_do_lists/view/header.php
@@ -2,11 +2,11 @@
 <html>
 <!-- the head section -->
 <head>
-    <title>My Guitar Shop</title>
+    <title>My To Do Lists</title>
     <link rel="stylesheet" type="text/css"
           href="/book_apps/ch05_guitar_shop/main.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My Guitar Shop</h1></header>
+<header><h1>My To Do Lists</h1></header>
